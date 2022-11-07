8c4db3ac586f15e90d71cecddf5359610277ac11
Stanislav <gsv9026@mail.ru>
2022-11-07 12:42:29 +0300

    create app

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let days =["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+let months = ['Январь' , 'Февраль' , 'Март' , 'Апрель' , 'Май' , 'Июнь' , 'Июль' , 'Август' , 'Сентябрь' , 'Октябрь' , 'Ноябрь' , 'Декабрь' ];
+let date = new Date();
+
+let data = document.querySelector('.data');
+let year = date.getFullYear();
+let month =  months[date.getMonth()];
+let day = date.getDate();
+let hour = date.getHours();
+let minute = date.getMinutes();
+
+data.innerHTML = year + ' ' +  month + ' ' + day + ' // ' + hour + ':' + minute;
+
+
